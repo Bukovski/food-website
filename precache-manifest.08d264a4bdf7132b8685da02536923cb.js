9b252a4889e079a83ba32a5788e4719462374228
Bukovski <kmanankov@bk.ru>
2020-03-03 11:27:00 +0300

    Updates

---
+++ precache-manifest.08d264a4bdf7132b8685da02536923cb.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a159e9ee6156eb14bd1b5c92e3a7b64",
+    "url": "/food-website/index.html"
+  },
+  {
+    "revision": "635b0018e21a5bea399c",
+    "url": "/food-website/static/css/main.6b482c11.chunk.css"
+  },
+  {
+    "revision": "9f6291296ec27225657b",
+    "url": "/food-website/static/js/2.3b0207f2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/food-website/static/js/2.3b0207f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "635b0018e21a5bea399c",
+    "url": "/food-website/static/js/main.15fd3bfd.chunk.js"
+  },
+  {
+    "revision": "697a4b535391e15080fe",
+    "url": "/food-website/static/js/runtime-main.bc6e1a5e.js"
+  },
+  {
+    "revision": "038b713377b413a522d9d15576f5a4b5",
+    "url": "/food-website/static/media/GentiumBookBasic-Bold.038b7133.svg"
+  },
+  {
+    "revision": "3eea259c9cfa31267750b6eca5b43725",
+    "url": "/food-website/static/media/GentiumBookBasic-Bold.3eea259c.woff2"
+  },
+  {
+    "revision": "5f812d68a66024ed69e426f3a19542bf",
+    "url": "/food-website/static/media/GentiumBookBasic-Bold.5f812d68.otf"
+  },
+  {
+    "revision": "aecb96109745a040d746b54de98cdc65",
+    "url": "/food-website/static/media/GentiumBookBasic-Bold.aecb9610.woff"
+  },
+  {
+    "revision": "d0ab1f74388723b9fcdb42d9c764bf4d",
+    "url": "/food-website/static/media/GentiumBookBasic-Bold.d0ab1f74.eot"
+  },
+  {
+    "revision": "d4fbcd4a7daab0353f5705b3f2b6f68b",
+    "url": "/food-website/static/media/GentiumBookBasic-Bold.d4fbcd4a.ttf"
+  },
+  {
+    "revision": "0f18d3f40f29e731232d202cd78c462f",
+    "url": "/food-website/static/media/YesevaOne-Regular.0f18d3f4.ttf"
+  },
+  {
+    "revision": "103cea7ab3089140baca86f62c16410b",
+    "url": "/food-website/static/media/YesevaOne-Regular.103cea7a.otf"
+  },
+  {
+    "revision": "1717ea251602b1d1316e114cb6e57c25",
+    "url": "/food-website/static/media/YesevaOne-Regular.1717ea25.woff"
+  },
+  {
+    "revision": "3497c8f51c02cbad40848d7e62526425",
+    "url": "/food-website/static/media/YesevaOne-Regular.3497c8f5.woff2"
+  },
+  {
+    "revision": "536d57605d2655da83e08cd1a6fea8d7",
+    "url": "/food-website/static/media/YesevaOne-Regular.536d5760.svg"
+  },
+  {
+    "revision": "73ae91322cca2e783ef50ceeb382aa5e",
+    "url": "/food-website/static/media/YesevaOne-Regular.73ae9132.eot"
+  },
+  {
+    "revision": "ecef6dc9dd2b31eb6f57fd3055f7ce25",
+    "url": "/food-website/static/media/bg-header.ecef6dc9.jpg"
+  },
+  {
+    "revision": "f15ba3e9b481b4349f9be12d8bed0edf",
+    "url": "/food-website/static/media/bg-ingredients.f15ba3e9.jpg"
+  },
+  {
+    "revision": "6bfa643e816e261d08ec6a7a4ac1d50c",
+    "url": "/food-website/static/media/bg-reviews.6bfa643e.jpg"
+  },
+  {
+    "revision": "f17dc99df2cb3314cce2623480273bd0",
+    "url": "/food-website/static/media/cook.f17dc99d.png"
+  },
+  {
+    "revision": "d9eab4eeb6949ff65f3c9395f85fd76b",
+    "url": "/food-website/static/media/dish.d9eab4ee.png"
+  },
+  {
+    "revision": "5eeeed004271c2ed4bb4f22b25cb90b6",
+    "url": "/food-website/static/media/food-1.5eeeed00.jpg"
+  },
+  {
+    "revision": "1692f2f8eb5ff7934d330e98c73da887",
+    "url": "/food-website/static/media/food-2.1692f2f8.jpg"
+  },
+  {
+    "revision": "96d7546f55ff572eeaeff12a570a50b2",
+    "url": "/food-website/static/media/ingradients.96d7546f.png"
+  },
+  {
+    "revision": "03fcc2b689dbbde1994eebd1ea7de296",
+    "url": "/food-website/static/media/latoregular.03fcc2b6.ttf"
+  },
+  {
+    "revision": "2e731727dd64d1832d52d86a78a48144",
+    "url": "/food-website/static/media/latoregular.2e731727.svg"
+  },
+  {
+    "revision": "642f2e3e6809ff2abe41ef0f981f644f",
+    "url": "/food-website/static/media/latoregular.642f2e3e.otf"
+  },
+  {
+    "revision": "d55d97821196a870f42e9484f6913aef",
+    "url": "/food-website/static/media/latoregular.d55d9782.woff2"
+  },
+  {
+    "revision": "eca0d5002baf1af9db4dfa8cda147ffa",
+    "url": "/food-website/static/media/latoregular.eca0d500.eot"
+  },
+  {
+    "revision": "edbfecab0454ac0e960e70a4e956325b",
+    "url": "/food-website/static/media/latoregular.edbfecab.woff"
+  }
+]);
